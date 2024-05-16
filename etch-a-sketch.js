76f1fdb9953992dfea7b48fd57ba26c4e185f0e4
Darron Dai <daidarron@gmail.com>
2024-05-15 18:14:38 -0700

    Move indicies display inside boxes instead of console logs

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,15 +2,16 @@ function createGrid(numRows, numColumns) {
   const container = document.querySelector(".grid-container");
 
   function createGridSquare() {
-    const squareDiv = document.createElement("div");
-    squareDiv.classList.add("grid-square");
-    return squareDiv;
+    const gridSquare = document.createElement("div");
+    gridSquare.classList.add("grid-square");
+    return gridSquare;
   }
 
   for (let row = 0; row < numRows; row++) {
     for (let col = 0; col < numColumns; col++) {
-      container.appendChild(createGridSquare());
-      console.log(`box ${row}, ${col}`);
+      const gridSquare = createGridSquare();
+      gridSquare.textContent = `${row}, ${col}`;
+      container.appendChild(gridSquare);
     }
   }
 }
